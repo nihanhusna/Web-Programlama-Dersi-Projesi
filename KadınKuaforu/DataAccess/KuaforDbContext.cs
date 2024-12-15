084a7a0b1a51eb63a7fc6087e0d7ea59fcd8ef5a
nihanhusna <nihanhkb@gmail.com>
2024-12-15 09:55:40 +0300

    veri tabanı işlemleri ve kütüphaneler

---
+++ KadınKuaforu/DataAccess/KuaforDbContext.cs
@@ -0,0 +1,17 @@
+﻿using KadınKuaforu.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KadınKuaforu.DataAccess
+{
+    public class KuaforDbContext : IdentityDbContext<Identity_User, Identity_Role, string>
+    {
+        public KuaforDbContext(DbContextOptions<KuaforDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+}

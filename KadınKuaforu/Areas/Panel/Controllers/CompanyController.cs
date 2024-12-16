1f7272123cf2a325b4dfc2752e039498bad49caf
nihanhusna <nihanhkb@gmail.com>
2024-12-16 23:08:14 +0300

    şirket modeli ve şirket repository sınıfı

---
+++ KadınKuaforu/Areas/Panel/Controllers/CompanyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KadınKuaforu.Areas.Panel.Controllers
+{
+    [Area("Panel")]
+    public class CompanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KadınKuaforu/DataAccess/CompanyRepository.cs
@@ -0,0 +1,16 @@
+﻿using KadınKuaforu.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KadınKuaforu.DataAccess
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly KuaforDbContext _dbContext;
+        private readonly DbSet<Company> _dbset;
+        public CompanyRepository(KuaforDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+            _dbset = _dbContext.Set<Company>();
+        }
+    }
+}

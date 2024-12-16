2d164a7607be85fc15a6bdd3bb93fa65db6190f6
nihanhusna <nihanhkb@gmail.com>
2024-12-16 23:08:14 +0300

    şirket modeli ve şirket repository sınıfı

---
+++ KadınKuaforu/DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace KadınKuaforu.DataAccess
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(string? includeProps = null);
+        T GetByIdWithProps(Expression<Func<T, bool>> filter, string? includeProps = null);
+        T GetById(int id);
+        IEnumerable<T> GetAllByCondition(Expression<Func<T, bool>> filter, string? includeProps = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}

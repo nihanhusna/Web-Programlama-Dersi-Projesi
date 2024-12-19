ca0633f6496da400ebcc75d41254285fbb3d1490
nihanhusna <nihanhkb@gmail.com>
2024-12-19 23:40:13 +0300

    kayıt, giriş, çıkış actionları yazıldı

--- KadınKuaforu/Models/Identity_User.cs
+++ KadınKuaforu/Models/Identity_User.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace KadınKuaforu.Models
 {
-    public class Identity_User : IdentityUser<string>
+    public class Identity_User : IdentityUser
     {
+        [Display(Name = "İsim Soyisim")]
+        [Required(ErrorMessage = "İsim Soyisim girmelisiniz")]
+        public string NameSurname { get; set; }
+        [Display(Name = "Hakkında")]
+        [Required(ErrorMessage = "Hakkında girmelisiniz")]
+        public string About { get; set; }
     }
 }
---
+++ KadınKuaforu/Models/SignInModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KadınKuaforu.Models
+{
+    public class SignInModel
+    {
+        [Display(Name ="Email")]
+        [EmailAddress(ErrorMessage ="Email doğru formatta girilmeli")]
+        public string Email { get; set; }
+
+        [Required (ErrorMessage ="Şifre girmelisiniz")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

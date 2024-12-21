0d24abe66147b2bd025db77f9c3fd50354e51c97
nihanhusna <nihanhkb@gmail.com>
2024-12-21 23:37:23 +0300

    Personeller, personel rütbeleri, rütbelerin görevleri, uzmanlıklar modelleri eklendi

---
+++ KadınKuaforu/KadınKuaforu/KadınKuaforu/Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KadınKuaforu.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name="Şirket İsmi")]
+        [Required(ErrorMessage ="Şirket isminizi girmelisiniz")]
+        public string Name { get; set; }
+        [Display(Name = "Şirket Adresi")]
+        [Required(ErrorMessage = "Şirket adresinizi girmelisiniz")]
+        public string Address { get; set; }
+        [Display(Name = "Şirket Numarası")]
+        [Phone(ErrorMessage ="Şirket telefon numaranızı doğru formatta giriniz")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Şirket Email Adresi")]
+        [EmailAddress(ErrorMessage ="Şirket Email Adresinizi doğru formatta giriniz")]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ KadınKuaforu/KadınKuaforu/KadınKuaforu/Models/Personnel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KadınKuaforu.Models
+{
+    public class Personnel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string IdentityUserId { get; set; }
+        [ForeignKey(nameof(IdentityUserId))]
+        public Identity_User Identity_User { get; set; }
+        public int RankId { get; set; }
+        [ForeignKey(nameof(RankId))]   
+        public Rank Rank { get; set; }
+        public ICollection<ExpertOfTask> ExpertOfTasks { get; set; }
+    }
+}
